ea621606fc881921ac6f4957712c7afec31038d4
airmen77 <airmen@ukr.net>
2022-07-01 12:53:07 +0300

    Add files via upload

---
+++ precache-manifest.12c779f75a43a3033582372fee5e09a1.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "42475228d179657e727d",
+    "url": "/js/chunk-vendors.e43d34fd.js"
+  },
+  {
+    "revision": "1ba1c86c8c7872ac57fc641ea94b5914",
+    "url": "/service-worker.js"
+  },
+  {
+    "revision": "4d9487de04b132ae822ca5409254be5b",
+    "url": "/img/phone.4d9487de.svg"
+  },
+  {
+    "revision": "a1b617796b7d1295029c",
+    "url": "/css/chunk-091a948d.6434f7cc.css"
+  },
+  {
+    "revision": "7fb2d4f6fa9ab5c5002e",
+    "url": "/css/chunk-1f98b0e1.6be31fc5.css"
+  },
+  {
+    "revision": "7fb2d4f6fa9ab5c5002e",
+    "url": "/js/chunk-1f98b0e1.b7a9b057.js"
+  },
+  {
+    "revision": "f7249eb74fcc2e5d3fdd",
+    "url": "/css/chunk-3bed1b79.798dcaca.css"
+  },
+  {
+    "revision": "f7249eb74fcc2e5d3fdd",
+    "url": "/js/chunk-3bed1b79.1c0cfb3f.js"
+  },
+  {
+    "revision": "fddf7b6e0e15b3b5e5b9",
+    "url": "/css/chunk-6c83ec2e.1665473d.css"
+  },
+  {
+    "revision": "fddf7b6e0e15b3b5e5b9",
+    "url": "/js/chunk-6c83ec2e.d0822538.js"
+  },
+  {
+    "revision": "4fd44cef370ba9d2f87e",
+    "url": "/js/app.f8c8c2a4.js"
+  },
+  {
+    "revision": "a1b617796b7d1295029c",
+    "url": "/js/chunk-091a948d.fdeeac7f.js"
+  },
+  {
+    "revision": "e530cabeed6af444668bece65129a26e",
+    "url": "/img/outline-store_mall_directory-24px.e530cabe.svg"
+  },
+  {
+    "revision": "1b97e34a8820985ff14aa882bf2d9960",
+    "url": "/img/outline-list-24px.1b97e34a.svg"
+  },
+  {
+    "revision": "c275e5189ecaabdbfdc5407dc031c28b",
+    "url": "/img/baseline-menu-24px.c275e518.svg"
+  },
+  {
+    "revision": "d96b304ddf33542c0fcb72d07b73d48a",
+    "url": "/img/outline-info-24px.d96b304d.svg"
+  },
+  {
+    "revision": "c0c003384035ba3df9647cb2d2d965b8",
+    "url": "/img/Spinner.c0c00338.gif"
+  },
+  {
+    "revision": "c915103363017d14cea9c2a46ea12166",
+    "url": "/img/apple-touch-icon.c9151033.png"
+  },
+  {
+    "revision": "aeeed97065093a36e08043d43cad4cf0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "4fd44cef370ba9d2f87e",
+    "url": "/css/app.3746a0c2.css"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,61 @@
+importScripts("/precache-manifest.12c779f75a43a3033582372fee5e09a1.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+console.log('PWA Service-worker')
+// Должно быть true в production
+var doCache = true
+// Имя кэша
+var CACHE_NAME = 'my-pwa-cache-v2'
+
+// Очищает старый кэш
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME]
+  event.waitUntil(
+    caches.keys()
+      .then(keyList =>
+        Promise.all(keyList.map(key => {
+          if (!cacheWhitelist.includes(key)) {
+            console.log('Deleting cache: ' + key)
+            return caches.delete(key)
+          }
+        }))
+      )
+  )
+})
+
+// 'install' вызывается, как только пользователь впервые открывает PWA
+self.addEventListener('install', function (event) {
+  if (doCache) {
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function (cache) {
+          // Получаем данные из манифеста (они кэшируются)
+          fetch('manifest.json')
+            .then(response => {
+              response.json()
+            })
+            .then(assets => {
+              // Открываем и кэшируем нужные страницы и файлы
+              const urlsToCache = [
+                '/',
+                '/history',
+                '/list'
+              ]
+              cache.addAll(urlsToCache)
+              console.log('cached')
+            })
+        })
+    )
+  }
+})
+
+// Когда приложение запущено, сервис-воркер перехватывает запросы и отвечает на них данными из кэша, если они есть
+self.addEventListener('fetch', function (event) {
+  if (doCache) {
+    event.respondWith(
+      caches.match(event.request).then(function (response) {
+        return response || fetch(event.request)
+      })
+    )
+  }
+})
+
